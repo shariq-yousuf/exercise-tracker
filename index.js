e29c0b93139803f75c875dcb2ccfe0f49b13382e
shariq-yousuf <shariqyousuf01@gmail.com>
2024-11-22 11:57:11 +0500

    feat: add GET & POST /api/users

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import { nanoid } from "nanoid"
 import dotenv from "dotenv"
 dotenv.config()
 
@@ -7,10 +8,31 @@ const app = express()
 
 app.use(cors())
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
   res.sendFile(import.meta.dirname + "/views/index.html")
 })
+
+const users = []
+
+app
+  .route("/api/users")
+  .post((req, res) => {
+    const username = req.body.username
+    const _id = nanoid()
+
+    const user = {
+      username,
+      _id,
+    }
+    users.push(user)
+
+    res.json(user)
+  })
+  .get((req, res) => {
+    res.json(users)
+  })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port)
